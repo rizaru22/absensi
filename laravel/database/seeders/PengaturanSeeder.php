e72dce2288032004a0cf80be58cc6b1fe831f91f
rizaru22 <rizaru.2.2@gmail.com>
2024-09-11 23:06:51 +0700

    tambah pengaturan seeder

---
+++ laravel/database/seeders/PengaturanSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class PengaturanSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+        DB::table('pengaturans')->insert([
+            'nama_instansi'=>'SMKN 1 Karang Baru',
+            'latitude'=>'4.2984487394942805',
+            'longitude'=>'98.04125699508066',
+            'jam_masuk'=>'06:30:00',
+            'jam_pulang'=>'12:30:00',
+            'jam_maksimal_masuk'=>'10:30:00',
+            'jam_maksimal_pulang'=>'18:30:00',
+            'jarak_maksimal'=>'170'
+        ]);
+    }
+}
