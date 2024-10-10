483cbeac146cd243f64921f7b227a664586e5d61
rizaru22 <rizaru.2.2@gmail.com>
2024-10-10 09:39:42 +0700

    fungsi notifikasi ke grup telegram berhasil dibuat

--- laravel/app/Http/Controllers/SendNotificationController.php
+++ laravel/app/Http/Controllers/SendNotificationController.php
@@ -5,31 +5,33 @@ namespace App\Http\Controllers;
 use Carbon\Carbon;
 use App\Models\User;
 use App\Models\Absensi;
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class SendNotificationController extends Controller
 {
     //
-    public function notifikasiMasuk(){
+    public function notifikasiMasuk()
+    {
+
         $tanggal = Carbon::now();
-        $nama=array();
+        $this->apiPesan("BELUM ABSEN MASUK " . $tanggal->isoFormat('DD MMMM Y'));
         $user = User::select('id', 'name')
             ->where('role', 'user')->orderBy('name')
             ->get()->toArray();
         foreach ($user as $us) {
             $absensi = Absensi::select('jam_masuk')->where('user_id', $us['id'])->whereDate('created_at', $tanggal)->get();
-            // dd($absensi,$us['name'],$tanggal);
             if (blank($absensi)) {
-                $nama[] = $us['name'];
+                $nama = $us['name'];
+                $this->apiPesan($nama);
             }
         }
-        $data['title'] = "Yang Belum Absen Masuk";
-        $data['nama'] = $nama;
+        $this->apiPesan("===============================");
     }
 
-    public function notifikasiPulang(){
+    public function notifikasiPulang()
+    {
         $tanggal = Carbon::now();
-        $nama=array();
+        $this->apiPesan("BELUM ABSEN PULANG " . $tanggal->isoFormat('DD MMMM Y'));
         $user = User::select('id', 'name')
             ->where('role', 'user')->orderBy('name')
             ->get()->toArray();
@@ -37,13 +39,20 @@ class SendNotificationController extends Controller
             $absensi = Absensi::select('jam_pulang')->where('user_id', $us['id'])->whereDate('created_at', $tanggal)->get()->toArray();
             // dd($absensi[0]->jam_pulang,$us['name'],$tanggal);
             if (blank($absensi)) {
-                $nama[] = $us['name'];
+                $nama = $us['name'];
             } elseif ($absensi[0]['jam_pulang'] == '0') {
-                $nama[] = $us['name'];
+                $nama = $us['name'];
             }
+            $this->apiPesan($nama);
         }
-        $data['title'] = "Yang Belum Absen Pulang";
-        $data['nama'] = $nama;
+        $this->apiPesan("===============================");
+    }
 
+    public function apiPesan($pesan)
+    {
+        Http::post('https://api.telegram.org/bot7241376794:AAHTp6dr5SywEgFVktN5LB3UPm2apn7hmqQ/sendMessage?', [
+            'chat_id' => '-4527049150',
+            'text' => $pesan
+        ]);
     }
 }
--- laravel/resources/views/pegawai/cuti.blade.php
+++ laravel/resources/views/pegawai/cuti.blade.php
@@ -17,15 +17,16 @@
                         <div class="form-group">
                             <label for="jam_masuk">Jenis Izin</label>
                             <select name="jam_masuk" id="jam_masuk" class="form-control" required>
-                            <option value="S">Sakit</option>
-                            <option value="I">Izin</option>
+                                <option value="DL">Dinas Luar</option>
+                                <option value="I">Izin</option>
+                                <option value="S">Sakit</option>
                             </select>
                         </div>
                         <div class="form-group">
-                        <label for="foto_izin">Bukti Dukung</label>
-                        <input type="file" id="foto_izin" class="form-control-file" name="foto_izin" accept="image/*" capture="user" / >
+                            <label for="foto_izin">Bukti Dukung</label>
+                            <input type="file" id="foto_izin" class="form-control-file" name="foto_izin" accept="image/*" capture="user" />
                         </div>
-                        
+
 
                         <div class="card-footer text-right mt-4">
 
