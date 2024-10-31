d1dcb3f9476ca7581e6f75209c7c8cc3ded55e26
rizaru22 <rizaru.2.2@gmail.com>
2024-10-31 21:18:58 +0700

    perbaikan element InputJarakPulang

--- laravel/resources/views/pegawai/dashboard.blade.php
+++ laravel/resources/views/pegawai/dashboard.blade.php
@@ -207,13 +207,14 @@
 
     <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
-    <script src="{{asset('dist/js/detect.js')}}"></script>
+    <!-- <script src="{{asset('dist/js/detect.js')}}"></script> -->
     <!-- Toastr -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
     <script>
         // const x = document.getElementById("keterangan");
   
         var inputJarak=document.getElementById("inputJarak");
+        var inputJarakPulang=document.getElementById("inputJarakPulang");
         let latitude;
         let longitude;
         var jarak;
@@ -292,7 +293,8 @@
             }
             inputJarak.value=jarak;
             inputJarakPulang.value=jarak;
-            document.getElementById('Jarak').innerHTML="Jarak: " +jarak+" meter"
+ 
+            document.getElementById('Jarak').innerHTML="Jarak: " +inputJarakPulang.value+" meter"
             // console.log(jarak)
             // var url = "{{route('masuk',':jarak')}}";
             // url = url.replace(':jarak', jarak);
