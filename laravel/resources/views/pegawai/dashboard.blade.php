99285bc66253a23539024533907fe36a7c417b82
rizaru22 <rizaru.2.2@gmail.com>
2024-11-20 13:59:12 +0700

    checkout to master

--- laravel/resources/views/pegawai/dashboard.blade.php
+++ laravel/resources/views/pegawai/dashboard.blade.php
@@ -205,7 +205,7 @@
 
     <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
-    <script src="{{asset('dist/js/detect.js')}}"></script>
+    <!-- <script src="{{asset('dist/js/detect.js')}}"></script> -->
     <!-- Toastr -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
     <script>
